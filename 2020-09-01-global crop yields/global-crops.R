5d6e84afbeceb264849d6f55e442d399dd9476bd
Owen Thompson <octhompson19@gmail.com>
2020-09-03 14:55:02 -0500

    Create global-crops.R

---
+++ 2020-09-01-global crop yields/global-crops.R
@@ -0,0 +1,16 @@
+# ---- Load libraries
+library(tidyverse)
+
+# ---- Read data
+key_crop_yields <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/key_crop_yields.csv')
+fertilizer <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/cereal_crop_yield_vs_fertilizer_application.csv')
+tractors <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/cereal_yields_vs_tractor_inputs_in_agriculture.csv')
+land_use <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/land_use_vs_yield_change_in_cereal_production.csv')
+arable_land <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/arable_land_pin.csv')
+
+
+glimpse(key_crop_yields)
+glimpse(fertilizer)
+glimpse(tractors)
+glimpse(land_use)
+glimpse(arable_land)
