523cd8419c2f1ec7c695e1e80b7ed1b1084a0408
Owen Thompson <octhompson19@gmail.com>
2021-07-23 15:15:41 -0500

    Create scoobydoo.R

---
+++ WIP/scoobydoo.R
@@ -0,0 +1,5 @@
+# -- Load libraries
+library(tidyverse)
+
+# -- Load data
+scoobydoo <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-13/scoobydoo.csv')
